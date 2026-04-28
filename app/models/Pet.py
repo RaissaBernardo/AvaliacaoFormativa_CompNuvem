b5563ed37f02d52baa6641547165823fba3486ff
RAÍSSA ANTONIA BERNARDO DE OLIVEIRA <raissa.oliveira13@senaisp.edu.br>
2026-04-28 19:23:52 -0300

    estrutura geral

---
+++ app/models/Pet.py
@@ -0,0 +1,14 @@
+#from app import db
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    tipo = db.Column(db.String(50))
+    raca = db.Column(db.String(50))
+    idade = db.Column(db.Integer)
+
+    cliente_id = db.Column(db.Integer, db.ForeignKey('clientes.id'), nullable=False)
+
+    atendimentos = db.relationship('Atendimento', backref='pet', lazy=True)
